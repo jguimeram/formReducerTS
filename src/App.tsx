c96a98ccc6362b68c7dd8f4f8714bf02e90d41d1
jguimeram <jguimera54@gmail.com>
2025-05-18 10:06:49 +0200

    form layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Form from "./components/Form"
 function App() {
 
 
   return (
     <>
-      <h1>Hello, World</h1>
+      <header className="w-full bg-emerald-400 text-black dark:dark:bg-gray-600 dark:text-white text-center text-4xl font-semibold p-5">
+        <h1 className="font-3xl">Form with React, Vite and TS</h1>
+      </header>
+      <section className="py-20 px-5 border-indigo-50">
+        <div className="max-w-4xl mx-auto">
+          <Form />
+        </div>
+      </section>
     </>
   )
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,27 @@
+function Form() {
+    return (
+        <form className="w-full text-black dark:text-white">
+            <div className="mb-5 grid grid-cols-1 gap-3">
+                <label htmlFor="category" className="font-medium dark:text-white">Category</label>
+                <select id="category"
+                    className="bg-gray-50 dark:bg-gray-700 rounded-lg border border-gray-300  dark:border-gray-600 p-2.5 w-full focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                    <option>Test</option>
+                </select>
+            </div>
+            <div className="mb-5 grid grid-cols-1 gap-3">
+                <label htmlFor="name" className="font-medium dark:text-white">Activity</label>
+                <input id="name"
+                    className="bg-gray-50 dark:bg-gray-700 rounded-lg border border-gray-300  dark:border-gray-600 p-2.5 w-full focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            </div>
+            <div className="mb-5 grid grid-cols-1 gap-3">
+                <label htmlFor="calories" className="font-medium dark:text-white">Calories</label>
+                <input id="calories"
+                    className="bg-gray-50 dark:bg-gray-700 rounded-lg border border-gray-300  dark:border-gray-600 p-2.5 w-full focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            </div>
+            <input type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" />
+        </form>
+    )
+}
+
+export default Form
